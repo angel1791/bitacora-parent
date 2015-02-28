582394828cfe3c5839dc557e7eab39cf5a18ee58
angel1791 <angel.florencia@habilgroup.net>
2015-02-28 13:34:00 -0600

    Revert "Revert "Creación de SpringTestCase""
    This reverts commit d9e9f44dc7eefa2ac71646e37d90c864f6d1899e.

---
+++ bitacora-model/main/java/mx/habil/model/dto/MensajeDto.java
@@ -0,0 +1,10 @@
+package mx.habil.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import mx.habil.support.HabilDto;
+
+public class MensajeDto extends HabilDto {
+
+	private @Getter @Setter String  mensaje;
+}
